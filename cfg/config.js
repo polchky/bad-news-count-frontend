cb38a6d3bdc16df76565e5d584b35b7c818d1acf
Pascal Gremaud <pascal.gremaud@unifr.ch>
2020-10-08 14:25:07 +0200

    Add run-time config

---
+++ cfg/config.js
@@ -0,0 +1,6 @@
+const config = {
+    backendURL: 'http://localhost:3000',
+};
+
+this.env = this.env || {};
+Object.assign(this.env, config);
--- src/assets/js/service.js
+++ src/assets/js/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://bad-news-api.decensor.yt';
+axios.defaults.baseURL = window.env.backendURL;
 
 export default {
 
